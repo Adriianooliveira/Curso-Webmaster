e6c9d9abaa96c0e06e80e86e74323ce2eac903bb
Adriano de Oliveira <adrianosilvasilvaa222@gmail.com>
2024-02-23 21:35:45 -0300

    nova aula adicionada

---
+++ JavaScript/condicoes e operadores/script.js
@@ -0,0 +1,39 @@
+window.onload = function() {
+
+    // ===  -> Alem de ser igual a variavel tem que ser do mesmo tipo!
+    // ==   -> Apenas o valor precisa ser igual.
+    // !=   -> diferente
+    // !==  -> diferente valor e tipo
+    // >=
+    // <=
+    // <
+    // >
+
+    var number = '23';
+
+    if(number !== 23) {
+        window.alert('verdadeiro');
+    } else {
+        window.alert('falso');
+    }
+
+
+
+
+
+
+
+
+    // var idade = 2024 - 2002
+    
+
+    // if(idade > 22) {
+    //     window.alert('Eu tenho mais de 22 anos de idade')
+    // } else if (idade < 10){
+    //     window.alert('Eu tenho menos que 10 anos')
+    // } else if (idade < 5) {
+    //     window.alert('Eu tenho menos que 5 anos')
+    // } else {
+    //     window.alert('Nenhuma das condições são verdadeiras')
+    // }
+}
---
+++ JavaScript/lacos de repeticoes/script.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+
+    var repeticao = 0;
+    var nome = 'Adriano'
+
+    //while precisamos de uma variavel inicial.
+
+    /*
+    while(repeticao <= 10); {
+        console.log('Olá mundo número ' + repeticao);
+        repeticao+=2
+    }
+    */
+
+
+    /*  while(nome == 'Adriano'); {
+        console.log('Olá mundo');
+        nome = 'João';
+    }
+
+    console.log('cheguei até o final');
+    */
+
+
+
+    /*
+    for(var numero = 0; numero <= 10; numero = numero + 1); {
+        console.log('Olá mundo');
+    }
+    */
+
+
+    do {
+        console.log('Olá mundo');
+    } while (nome == 'Adriano')
+
+
+}
