f72dc3ef58c6707b0d2f421c3d28cae20bf7460a
Adriano de Oliveira <adrianosilvasilvaa222@gmail.com>
2024-02-24 10:50:34 -0300

    nova aula adicionada

---
+++ JavaScript/funcoes/script.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+  
+    /*
+    var variavel = function() {
+        console.log("Minha funcao atraves de variavel");
+    }
+    */
+
+
+
+
+
+    /*
+    function teste(num1,num2) {
+        return num1 + num2
+    }
+
+     var variavel = teste(10,20)
+
+     console.log(variavel)
+    */
+
+
+
+    /*
+    var obj = {'nome':'Adriano', 'idade':21, 'func':function(){
+        console.log("Adriano funcao")
+    }};
+
+        console.log(obj.nome, obj.idade);
+
+        obj.func();
+    */
+
+    /*
+    var obj = {'nome':'Adriano', 'idade':21}
+
+    obj.func = function() {
+        console.log('minha funcao');
+    }
+
+    obj.func();
+    */
+
+
+    /*
+    var variavel = [];
+
+    variavel[0] = [];
+
+    variavel[0][2] = 'Adriano';
+
+    console.log(variavel[0][2]);
+    */
+
+    var variavel = [];
+
+    variavel.obj = {}
+
+    variavel.obj.nome = 'Adriano'
+
+    console.log(variavel.obj.nome)
+}
