448a1200e0e05a203c5cd2fe36494fde14a7c372
Adriano de Oliveira <adrianosilvasilvaa222@gmail.com>
2024-02-27 21:21:25 -0300

    aula de jávascript atualizada

--- projetos/projeto3/js/script.js
+++ projetos/projeto3/js/script.js
@@ -13,11 +13,10 @@ window.onload = function() {
         map = new google.maps.Map(document.getElementById('map'),mapProp)
     }
 
+    function addMarker(lat,long,icon,content,click) {
+        var latLng = {'lat':lat,'lng':long};
 
-    function addMarker(lat,long,icon,content) {
-        var latLng = {'lat':lat,'long':long}
-
-        var marker = new google.maps.Marker({
+        var marker =  new google.maps.Marker({
             position:latLng,
             map:map,
             icon:icon
@@ -26,15 +25,30 @@ window.onload = function() {
         var infoWindow = new google.maps.InfoWindow({
             content:content,
             maxWidth:200,
-            pixelOffset: new google.maps.Size(0,20)
+            pixelOffset: new google.maps.Size(0,10)
         });
 
+        if(click == true) {
+        google.maps.event.addListener(marker,'click',function() {
+            infoWindow.open(map,marker);
+        });
         infoWindow.open(map,marker)
+        }  else {
+            infoWindow.open(map,marker)
+        }
     }
 
 
     initialize();
-    addMarker(-22.928316,-51.7173602)
+
+    var conteudo = '<p style="color: black; font-size: 14px; border: 0; padding: 5px;  border-bottom: 1px solid black;">Meu endereço</p>'
+    addMarker(-22.928316,-51.71736051,'',conteudo, true)
+
+    setTimeout(function() {
+        map.panTo({'lat':-22.906847, 'lng':-43.172897});
+        map.setZoom(8)
+    },4000)
+
 
     /*
     function animal() {
