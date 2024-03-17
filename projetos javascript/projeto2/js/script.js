1f964e86cda86adc17119cd76986a41c537ac046
Adriano de Oliveira <adrianosilvasilvaa222@gmail.com>
2024-03-16 23:14:56 -0300

    novo projeto adicionado

---
+++ projetos javascript/projeto2/js/script.js
@@ -0,0 +1,27 @@
+const items = [
+    {
+        id: 0,
+        nome: 'camiseta',
+        img: 'image.jpg',
+        quantidade: 0,
+    },
+
+    {
+        id: 1,
+        nome: 'short',
+        img: 'image.jpg',
+        quantidade: 0,
+    },
+
+    {
+        id: 2,
+        nome: 'sapatos',
+        img: 'image.jpg',
+        quantidade: 0,
+    },
+
+]
+
+function inicializarLoja() {
+    var documentsprodutos = window.document.getElementById
+}
