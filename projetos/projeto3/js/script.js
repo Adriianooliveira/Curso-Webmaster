4b01c23d8d5bf1abdb8d60a7f720ea3d41349b96
Adriano de Oliveira <adrianosilvasilvaa222@gmail.com>
2025-01-11 22:01:30 -0300

    mudanças no site

--- projetos/projeto3/js/script.js
+++ projetos/projeto3/js/script.js
@@ -1,53 +1,32 @@
-window.onload = function() {
 
-    var map;
-
-    function initialize() {
-        var mapProp = {
-            center: new google.maps.LatLng(-22.928316,-51.7173605),
-            scrolwhell:false,
-            zoom:12,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-
-        map = new google.maps.Map(document.getElementById('map'),mapProp)
+async function initMap() {
+    const { Map } = await google.maps.importLibrary("maps");
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    const map = new Map(document.getElementById("map"), {
+    center: { lat: 37.39094933041195, lng: -122.02503913145092 },
+    zoom: 14,
+    mapId: "4504f8b37365c3d0",
+    });
+    
+    const draggableMarker = new AdvancedMarkerElement({
+    map,
+    position: { lat: 37.39094933041195, lng: -122.02503913145092 },
+    gmpDraggable: true,
+    title: "This marker is draggable. Click to remove.",
+    });
+    
+    draggableMarker.addListener("click", (event) => {
+    // Remove AdvancedMarkerElement from Map
+    draggableMarker.map = null;
+    });
+    map.addListener("click", (event) => {
+    // Set AdvancedMarkerView position and add to Map
+    draggableMarker.position = event.latLng;
+    draggableMarker.map = map;
+    });
     }
-
-    function addMarker(lat,long,icon,content,click) {
-        var latLng = {'lat':lat,'lng':long};
-
-        var marker =  new google.maps.Marker({
-            position:latLng,
-            map:map,
-            icon:icon
-        });
-
-        var infoWindow = new google.maps.InfoWindow({
-            content:content,
-            maxWidth:200,
-            pixelOffset: new google.maps.Size(0,10)
-        });
-
-        if(click == true) {
-        google.maps.event.addListener(marker,'click',function() {
-            infoWindow.open(map,marker);
-        });
-        infoWindow.open(map,marker)
-        }  else {
-            infoWindow.open(map,marker)
-        }
-    }
-
-
-    initialize();
-
-    var conteudo = '<p style="color: black; font-size: 14px; border: 0; padding: 5px;  border-bottom: 1px solid black;">Meu endereço</p>'
-    addMarker(-22.928316,-51.71736051,'',conteudo, true)
-
-    setTimeout(function() {
-        map.panTo({'lat':-22.906847, 'lng':-43.172897});
-        map.setZoom(8)
-    },4000)
+    
+    initMap();
 
 
     /*
@@ -61,5 +40,3 @@ window.onload = function() {
     var meuanimal = new animal()
     console.log(meuanimal.nome)
     */
-
-}
